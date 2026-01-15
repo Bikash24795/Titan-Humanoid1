5efbed2446403f1e12705562f31d2506bcfc2ade
Bikash24795 <116132049+Bikash24795@users.noreply.github.com>
2026-01-15 11:29:53 +0530

    Update titan_main.py

--- controllers/titan_main/titan_main/titan_main.py
+++ controllers/titan_main/titan_main/titan_main.py
@@ -1,19 +1,22 @@
 import os
 from controller import Robot
 
-# This tells Python to talk to the local Webots instance
-os.environ['WEBOTS_ROBOT_NAME'] = 'Titan'
-
+# The brain connecting to the body
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
-# Initialize Motors
+# Get devices
 r_hip = robot.getDevice('RHip')
 l_hip = robot.getDevice('LHip')
 
+print("--- CONTROLLER CONNECTED TO TITAN SUCCESSFUL ---")
+
 while robot.step(timestep) != -1:
-    # Make the robot "dance" slightly to test the motors
     import math
     val = math.sin(robot.getTime()) * 0.5
     r_hip.setPosition(val)
     l_hip.setPosition(-val)
+    
+    # ADD THIS LINE TO SEE PROGRESS IN COLAB:
+    if int(robot.getTime()) % 2 == 0: 
+        print(f"Robot Time: {robot.getTime():.2f} - Hips moving to {val:.2f}")
